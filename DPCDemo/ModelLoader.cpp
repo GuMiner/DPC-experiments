45798b3725cc79736ae2217d444caaf33016dec7
GuMiner <gus.gran@gmail.com>
2021-01-23 01:23:53 -0800

    Centroid and volume results for a rigid body.

--- DPCDemo/ModelLoader.cpp
+++ DPCDemo/ModelLoader.cpp
@@ -9,8 +9,10 @@
 #pragma clang diagnostic ignored "-Wreorder-ctor"
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #pragma clang diagnostic ignored "-Wformat"
-#include <igl/opengl/glfw/Viewer.h>
+//#include <igl/opengl/glfw/Viewer.h>
 #pragma clang diagnostic pop
+
+#include <igl/centroid.h>
 
 #include "ModelLoader.h"
 
@@ -20,7 +22,17 @@ void ModelLoader::LoadAndRender()
     Eigen::MatrixXd N;
     Eigen::MatrixXi F;
     igl::readSTL("test-fans/plane.stl", V, F, N);
-    igl::opengl::glfw::Viewer viewer;
-    viewer.data().set_mesh(V, F);
-    viewer.launch();
+
+    //Eigen::SparseMatrix<double> M;
+    //igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+    Eigen::Vector3d center;
+    double volume;
+    igl::centroid(V, F, center, volume);
+    //igl::barycenter(V, F, BC);
+    std::cout << center << std::endl;
+    std::cout << volume << std::endl;
+    std::cout << "hi" << std::endl;
+    //igl::opengl::glfw::Viewer viewer;
+    //viewer.data().set_mesh(V, F);
+    //viewer.launch();
 }
